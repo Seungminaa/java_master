dc01a4e704f5fd25cd918d21b1b010118a91631d
admin <admin@DESKTOP-IPNJD32>
2023-12-07 17:30:48 +0900

    12/07

---
+++ hgjava/src/baekjoon/w1769.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class w1769 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String a = br.readLine();
+		String[] b = new String[a.length()+1];
+		for(int i=0;i<a.length();i++) {
+			b[i]=a.substring(i, i+1);
+		}
+		
+	}
+	static int exe(int[] b) {
+		
+		
+		return 1;
+	}
+}
